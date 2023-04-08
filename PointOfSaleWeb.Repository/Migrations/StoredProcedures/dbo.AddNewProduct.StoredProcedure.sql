6bc6a49657f77732a87fa7a931146863e5cfb2e5
Luis_Eduardo <undernet1@gmail.com>
2023-04-08 11:45:27 -0400

    Modifed error

--- PointOfSaleWeb.Repository/Migrations/StoredProcedures/dbo.AddNewProduct.StoredProcedure.sql
+++ PointOfSaleWeb.Repository/Migrations/StoredProcedures/dbo.AddNewProduct.StoredProcedure.sql
@@ -1,55 +1,72 @@
 USE [Inventory]
 GO
-    /****** Object:  StoredProcedure [dbo].[AddNewProduct]    Script Date: 4/6/2023 10:13:18 AM ******/
+
+/****** Object:  StoredProcedure [dbo].[AddNewProduct]    Script Date: 4/8/2023 11:30:12 AM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
-GO ALTER PROCEDURE [dbo].[AddNewProduct] @ProductID INT = 0,
-    @ProductName nvarchar(50),
-    @ProductDescription nvarchar(100) = NULL,
-    @ProductPrice decimal(10, 2),
-    @ProductCost decimal(10, 2),
-    @ProductStock int,
-    @ProductCategoryID int AS BEGIN
-SET NOCOUNT ON;
-IF EXISTS (
-    SELECT 1
-    FROM [dbo].[Products]
-    WHERE [ProductName] = @ProductName
-) BEGIN RAISERROR (
-    'Product %s already exists!',
-    16,
-    1,
-    @ProductName
-);
-RETURN;
-END BEGIN TRY;
-BEGIN TRANSACTION;
-INSERT INTO [dbo].[Products] (
-        ProductName,
-        ProductDescription,
-        ProductPrice,
-        ProductCost,
-        ProductStock,
-        ProductCategoryID
-    )
-VALUES (
-        @ProductName,
-        @ProductDescription,
-        @ProductPrice,
-        @ProductCost,
-        @ProductStock,
-        @ProductCategoryID
-    );
-COMMIT TRANSACTION;
-SELECT @ProductID = SCOPE_IDENTITY();
-EXEC GetProductById @ProductID = @ProductID;
-END TRY BEGIN CATCH;
-IF (@@TRANCOUNT > 0) BEGIN;
-ROLLBACK TRANSACTION;
-END;
-PRINT 'Error ocurred in ' + ERROR_PROCEDURE() + ' ' + ERROR_MESSAGE();
-RETURN -1;
-END CATCH;
-RETURN 0;
-END;
+GO
+
+ALTER PROCEDURE [dbo].[AddNewProduct]
+    @ProductID INT = 0,
+    @ProductName NVARCHAR(50),
+    @ProductDescription NVARCHAR(100) = NULL,
+    @ProductPrice DECIMAL(10, 2),
+    @ProductCost DECIMAL(10, 2),
+    @ProductStock INT,
+    @ProductCategoryID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	  IF EXISTS (SELECT 1
+           FROM [dbo].[products]
+           WHERE [productname] = @ProductName)
+BEGIN
+    THROW 50000, 'Product name already exists!', 1;
+END
+
+  
+    IF NOT EXISTS (SELECT 1
+                   FROM categories
+                   WHERE categoryid = @ProductCategoryID)
+    BEGIN
+        THROW 51000, 'Product category does not exist!', 1;
+    END
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        INSERT INTO [dbo].[products]
+               (productname,
+                productdescription,
+                productprice,
+                productcost,
+                productstock,
+                productcategoryid)
+        VALUES (@ProductName,
+                @ProductDescription,
+                @ProductPrice,
+                @ProductCost,
+                @ProductStock,
+                @ProductCategoryID);
+
+        COMMIT TRANSACTION;
+
+        SELECT @ProductID = SCOPE_IDENTITY();
+
+        EXEC GetProductByID @ProductID = @ProductID;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        DECLARE @ErrorMessage NVARCHAR(MAX);
+        SET @ErrorMessage = 'Error occurred in ' + ERROR_PROCEDURE() + ': ' + ERROR_MESSAGE();
+
+        THROW 51001, @ErrorMessage, 1;
+    END CATCH;
+
+    RETURN 0;
+END
