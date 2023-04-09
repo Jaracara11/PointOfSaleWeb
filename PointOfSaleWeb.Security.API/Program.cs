c699f82ae15b5844a30970840a9d9cb598dce97c
Luis_Eduardo <undernet1@gmail.com>
2023-04-09 09:42:06 -0400

    Working in the security API

---
+++ PointOfSaleWeb.Security.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseCors("CorsPolicy");
+
+app.Run();
+
