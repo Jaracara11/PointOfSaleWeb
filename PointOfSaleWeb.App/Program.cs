03a60cce03a72606139533a5b3a29894422642a2
Luis_Eduardo <undernet1@gmail.com>
2023-04-16 20:30:41 -0400

    Added swagger back to project

--- PointOfSaleWeb.App/Program.cs
+++ PointOfSaleWeb.App/Program.cs
@@ -37,7 +37,16 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
