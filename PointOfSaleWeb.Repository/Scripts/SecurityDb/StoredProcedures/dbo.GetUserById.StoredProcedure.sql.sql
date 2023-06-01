41c04e84135a41b8b7304e23a6828e50d31fc672
Luis_Eduardo <undernet1@gmail.com>
2023-06-01 16:23:34 -0400

    Added new stored procedure to retrieve userByID

---
+++ PointOfSaleWeb.Repository/Scripts/SecurityDb/StoredProcedures/dbo.GetUserById.StoredProcedure.sql.sql
@@ -0,0 +1,23 @@
+﻿USE [POS]
+GO
+SET
+    ANSI_NULLS ON
+GO
+SET
+    QUOTED_IDENTIFIER ON
+GO
+    CREATE PROCEDURE [dbo].[GetUserById] @UserId INT 
+    AS 
+    BEGIN
+SELECT
+    Username,
+    FirstName,
+    LastName,
+    Email,
+    UserRoleID
+FROM
+    Users WITH (NOLOCK)
+WHERE
+    UserID = @UserId
+END
+GO
