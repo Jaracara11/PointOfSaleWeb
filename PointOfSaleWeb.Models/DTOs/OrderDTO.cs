d853883b32f3a96b101f0e89c19d5c7d1c9cc7a7
Luis_Eduardo <undernet1@gmail.com>
2023-06-24 21:11:53 -0400

    .

--- PointOfSaleWeb.Models/DTOs/OrderDTO.cs
+++ PointOfSaleWeb.Models/DTOs/OrderDTO.cs
@@ -4,7 +4,7 @@
     {
         public string OrderID { get; set; } = string.Empty;
         public string User { get; set; } = string.Empty;
-        public string[] Products { get; set; } = Array.Empty<string>();
+        public string Products { get; set; } = string.Empty;
         public decimal OrderSubTotal { get; set; }
         public decimal? Discount { get; set; }
         public decimal OrderTotal { get; set; }
