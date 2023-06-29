c02d1911b339e1769063c61416042e6893102af5
Luis_Eduardo <undernet1@gmail.com>
2023-06-29 19:08:38 -0400

    .

--- PointOfSaleWeb.Models/DTOs/RecentOrdersDTO.cs
+++ PointOfSaleWeb.Models/DTOs/RecentOrdersDTO.cs
@@ -3,7 +3,6 @@
     public class RecentOrdersDTO
     {
         public string User { get; set; } = string.Empty;
-        public string Products { get; set; } = string.Empty;
         public decimal OrderTotal { get; set; }
         public DateTime OrderDate { get; set; }
     }
