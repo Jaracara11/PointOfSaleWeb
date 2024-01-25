f44ff95bc7089245bdfe64ddaf8b444fd5cf9c15
Luis_Eduardo <undernet1@gmail.com>
2024-01-25 11:09:22 -0400

    Added TotalUnitsSold to the ProductSoldByDateDTO

--- PointOfSaleWeb.Models/DTOs/ProductSoldByDateDTO.cs
+++ PointOfSaleWeb.Models/DTOs/ProductSoldByDateDTO.cs
@@ -5,6 +5,7 @@
         public string ProductID { get; set; } = string.Empty;
         public string ProductName { get; set; } = string.Empty;
         public string ProductDescription { get; set; } = string.Empty;
+        public int TotalUnitsSold { get; set; }
         public decimal TotalSold { get; set; }
     }
 }
