0d32218bee25bf00f475ef8f04699d55ad7ca7b5
Luis Eduardo <Luis.Eduardo@amtrustgroup.com>
2024-03-28 16:44:09 -0400

    .

--- deploy_pos_app.sh
+++ deploy_pos_app.sh
@@ -6,6 +6,8 @@ set -e
 GIT_BRANCH="master"
 DOCKER_IMAGE_NAME="pos.app"
 CONTAINER_NAME="pos-web.app"
+HOST_PORT_HTTP=8080
+HOST_PORT_HTTPS=8443
 
 # Deployment Steps
 cd PointOfSaleWeb.App
@@ -15,7 +17,7 @@ git pull
 sudo docker stop "$CONTAINER_NAME" || true  # Ignore errors if container doesn't exist
 sudo docker rm "$CONTAINER_NAME" || true    # Ignore errors if container doesn't exist
 sudo docker build -t "$DOCKER_IMAGE_NAME" .
-sudo docker run -d --restart unless-stopped --name "$CONTAINER_NAME" "$DOCKER_IMAGE_NAME"
+sudo docker run -d -p "$HOST_PORT_HTTP:80" -p "$HOST_PORT_HTTPS:443" --restart unless-stopped --name "$CONTAINER_NAME" "$DOCKER_IMAGE_NAME"
 
 # Cleanup
 sudo docker system prune -af   # Remove all stopped containers, unused networks, and dangling images
